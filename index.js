15d0a4c4f9187a93d7ac1a4bec426e4c83253385
thefjrm <felipemartinsplayernet@gmail.com>
2021-11-30 23:29:18 -0300

    Create the Dice game

---
+++ index.js
@@ -0,0 +1,62 @@
+let player1Score = 0;
+let player2Score = 0;
+let player1Turn = true;
+const player1Dice = document.getElementById("player1Dice");
+const player2Dice = document.getElementById("player2Dice");
+const player1Scoreboard = document.getElementById("player1Scoreboard");
+const player2Scoreboard = document.getElementById("player2Scoreboard");
+const message = document.getElementById("message");
+const rollBtn = document.getElementById("rollBtn");
+const resetBtn = document.getElementById("resetBtn");
+
+function showDisplayButton() {
+  rollBtn.style.display = "none";
+  resetBtn.style.display = "block";
+}
+
+rollBtn.addEventListener("click", function () {
+  const randomNumber = Math.floor(Math.random() * 6) + 1;
+  if (player1Turn) {
+    player1Score += randomNumber;
+    player1Scoreboard.textContent = player1Score;
+    player1Dice.textContent = randomNumber;
+    player1Dice.classList.remove("active");
+    player2Dice.classList.add("active");
+    message.textContent = "Player 2's Turn";
+  } else {
+    player2Score += randomNumber;
+    player2Scoreboard.textContent = player2Score;
+    player2Dice.textContent = randomNumber;
+    player2Dice.classList.remove("active");
+    player1Dice.classList.add("active");
+    message.textContent = "Player 1's Turn";
+  }
+
+  if (player1Score >= 20) {
+    message.textContent = "Player 1 Wins!";
+    showDisplayButton();
+  } else if (player2Score >= 20) {
+    message.textContent = "Player 2 Wins!";
+    showDisplayButton();
+  }
+  player1Turn = !player1Turn;
+});
+
+function reset() {
+  message.textContent = "Player 1 Turn";
+  player1Score = 0;
+  player2Score = 0;
+  player1Turn = true;
+  player1Scoreboard.textContent = 0;
+  player2Scoreboard.textContent = 0;
+  player1Dice.textContent = "-";
+  player2Dice.textContent = "-";
+  resetBtn.style.display = "none";
+  rollBtn.style.display = "block";
+  player2Dice.classList.remove("active");
+  player1Dice.classList.add("active");
+}
+
+resetBtn.addEventListener("click", function () {
+  reset();
+});
